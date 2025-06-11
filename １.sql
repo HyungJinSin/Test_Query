5480e404288d2653f2ea2ddfac1beb90d00984d7
HyungJinSin <74947204+HyungJinSin@users.noreply.github.com>
2025-06-11 13:55:18 +0900

    Add files via upload

---
+++ １.sql
@@ -0,0 +1,15 @@
+--SELECT * FROM A_ACCT_CFS_IF;
+
+--SELECT * FROM _SBElement;
+
+--SELECT * FROM CFS_INFO;
+
+
+--select * from ESS_COMPANY_CAR_MASTER;
+
+select * from I_LOT_MASTER;
+
+--SELECT * FROM J_LOTINFO;
+
+
+select * from B_LANGUAGE;
